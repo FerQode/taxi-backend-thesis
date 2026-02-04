273610f99dbb2bd2d44b381b5cb1bba607e4f46b
Romel Quinapallo <romelquinapallo12@gmail.com>
2026-02-04 09:42:52 -0500

    feat: Add init_roles command and update Procfile

--- src/infrastructure/django_conf/settings.py
+++ src/infrastructure/django_conf/settings.py
@@ -142,10 +142,23 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+# CORS
+CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', default='http://localhost:3000,http://127.0.0.1:3000').split(',')
+CORS_ALLOW_CREDENTIALS = True
+
+LANGUAGE_CODE = 'es-ec'
+TIME_ZONE = config('TZ', default='America/Guayaquil')
 USE_I18N = True
 USE_TZ = True
+
+# Email Configuration (Optional - Ready for Prod)
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default=EMAIL_HOST_USER)
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
